2960512aa854846d78257ee1ed1851407f46ecfe
Slydite <slydite@gmail.com>
2024-03-28 21:04:57 +0530

    import apexcharts

--- microfront/src/views/AboutView.vue
+++ microfront/src/views/AboutView.vue
@@ -1,8 +1,39 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <!-- Add a div element for the chart with an id -->
+    <div id="chart"></div>
   </div>
 </template>
+
+<script>
+// Import the necessary ApexCharts components
+import ApexCharts from 'apexcharts'
+
+export default {
+  name: 'About',
+  // Use the mounted lifecycle hook to render the chart when the component is mounted
+  mounted() {
+    var options = {
+      chart: {
+        type: 'line',
+        height: 'auto'
+      },
+      series: [{
+        name: 'sales',
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+      }],
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    }
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+    chart.render();
+  }
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
@@ -10,6 +41,8 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    flex-direction: column;
+    /* Add this to align items vertically */
   }
 }
 </style>
