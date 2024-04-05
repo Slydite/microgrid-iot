a745fd858285f6d8315e0dcd6aa562fc5c2cc63d
SominR <somin.ranjan@gmail.com>
2024-04-05 23:16:12 +0530

    apollo client setup

--- microfront/src/main.ts
+++ microfront/src/main.ts
@@ -1,14 +1,43 @@
-import './assets/main.css'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://127.0.0.1:8000/graphql/',
+})
 
-import App from './App.vue'
-import router from './router'
+// Cache implementation
+const cache = new InMemoryCache()
 
-const app = createApp(App)
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+const app=createApp({
+    setup() {
+        provide(DefaultAppolloClient, apolloClient)
+        
+    },
+    render: () => h(App)
+    
 
-app.use(createPinia())
-app.use(router)
+})
 
 app.mount('#app')
+
+
+import './assets/main.css'
+
+//import { createApp } from 'vue'
+//import { createPinia } from 'pinia'
+
+//import App from './App.vue'
+//import router from './router'
+
+//const app = createApp(App)
+
+//app.use(createPinia())
+//app.use(router)
+
+//app.mount('#app')
