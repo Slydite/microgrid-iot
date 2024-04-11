dac3acc6d6eb9ed07c2a74040cb37f263781c45a
Slydite <slydite@gmail.com>
2024-04-12 02:35:52 +0530

    fix merge

--- microback/microgrid_back/models.py
+++ microback/microgrid_back/models.py
@@ -8,3 +8,35 @@ class MeasurementsOne(models.Model):
         return f"Voltage: {self.voltage}, Time: {self.time}"
 
 
+class MeasurementsTwo(models.Model):
+    sensor_id = models.PositiveIntegerField()
+    voltage = models.DecimalField(max_digits=5, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Voltage: {self.voltage}, Time: {self.time}, Sensor ID: {self.sensor_id}"
+
+class MeasurementsThree(models.Model):
+    sensor_id = models.PositiveIntegerField()
+    voltage = models.DecimalField(max_digits=5, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Voltage: {self.voltage}, Time: {self.time}, Sensor ID: {self.sensor_id}"
+class MeasurementsFour(models.Model):
+    sensor_id = models.PositiveIntegerField()
+    voltage = models.DecimalField(max_digits=5, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Voltage: {self.voltage}, Time: {self.time}, Sensor ID: {self.sensor_id}"
+class MeasurementsFive(models.Model):
+    sensor_id = models.PositiveIntegerField()
+    voltage = models.DecimalField(max_digits=5, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Voltage: {self.voltage}, Time: {self.time}, Sensor ID: {self.sensor_id}"
+
+class MeasurementsSix(models.Model):
+    sensor_id = models.PositiveIntegerField()
+    voltage = models.DecimalField(max_digits=5, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Voltage: {self.voltage}, Time: {self.time}, Sensor ID: {self.sensor_id}"
